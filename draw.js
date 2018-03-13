28f3ddc0e59a4e55dac1e150e46f696785122025
Andekl <andreas.eklund94@gmail.com>
2018-03-13 19:29:16 +0100

    finished the draw module

--- draw.js
+++ draw.js
@@ -37,6 +37,77 @@ var drawModule (function(){
     }
   }
 
+  var paint = function(){
+    //Draw the space where the snake moves
+    ctx.fillStyle = 'lightgrey';
+    ctx.fillRect(0,0,w,h);
+
+    //Border
+    ctx.strokeStyle = 'black';
+    ctx.strokeRect(0,0,w,h);
+
+    //Disable the start button while playing
+    btn.setAttribute('disabled', true);
+
+    var snakeX = snake[0].x;
+    var snakeY = snake[o].y;
+
+    //Making the snake move using the 'direction' variable
+    if(direction === 'right'){
+      snakeX++;
+    }
+    else if (direction === 'left'){
+      snakeX--;
+    }
+    else if (direction === 'up'){
+      snakeY--;
+    }
+    else if (direction === 'down'){
+      snakeY++;
+    }
+
+    //Stop the game if the snake touches the border
+    if (snakeX == -1 || snakeX == w / snakeSize || snakeY == -1 || snakeY == h / snakeSize || check_collision(snakeX, snakeY, snake))
+    {
+    btn.removeAttribute('disabled', true);
+
+    ctx.clearRect(0,0,w,h);
+    gameloop = clearInterval(gameloop);
+    return;
+    }
+
+    //Eating food and growing function
+    if (snakeX == food.x && snakeY == food.y){
+      //create a new square
+      var tail = {
+        x: snakeX,
+        y: snakeY
+      };
+      score++;
+      createFood();
+    }
+    else {
+      var tail = snake.pop()
+      tail.x = snakeX;
+      tail.y = snakeY;
+    }
+
+    //Put the tail as the first cell
+    snake.unshift(tail);
+
+    for(var i = 0; i < snake.length;i++){
+      bodySnake(snake[i].x, snake[i].y);
+    }
+
+    //Create food
+    pizza(food.x, food.y);
+
+    //Score
+    scoreText();
+
+
+  }
+
   var createFood = function(){
     food = {
       //Generate random numbers
@@ -65,7 +136,19 @@ var drawModule (function(){
       return false;
     }
 
+   var init = function () {
+      direction = 'down';
+      drawSnake();
+      createFood();
+      gameloop = setInterval(paint, 80);
+  }
+
+  //You need to return only the _init_ function at the end of the Module.
+  return {
+      init: init
+  };
 
 
 
-})
+
+}());
